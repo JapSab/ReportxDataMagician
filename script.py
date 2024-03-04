a98dbffeac8ae2918ada73d9e1189d31d63e5beb
JapSab <116835013+JapSab@users.noreply.github.com>
2024-03-04 15:33:28 +0400

    Update script.py

--- script.py
+++ script.py
@@ -6,5 +6,5 @@ df = pd.read_excel('excelFIle.xlsx')
 date_columns = ['StartDate', 'EndDate']
 df[date_columns] = df[date_columns].apply(pd.to_datetime, format='%d/%m/%Y').apply(lambda x: x.dt.strftime('%Y/%m/%d'))
 
-df['value_gel'] = df.apply(lambda row: row['Value'] * 1000 if row['GEL'] == '000.ლარი' else row['Value'], axis=1)
+df['value_gel'] = df.apply(lambda row: row['Value'] * 1000 if row['GEL'] == '.000 ლარი' else row['Value'], axis=1)
 df.to_csv('CSVFile.csv', sep='|', index=False)
